e109d97649443232d57858f2a1d9eaab1ae410ca
Chris Ohk <utilforever@gmail.com>
2021-07-12 23:07:02 +0900

    feat: Add code to identify platform 'win32' and 'darwin'

--- PyCorailed/capture/window.py
+++ PyCorailed/capture/window.py
@@ -1,7 +1,12 @@
 import cv2
 import numpy as np
-import win32gui
 import pyautogui
+from sys import platform
+
+if platform == "win32":
+    import win32gui
+elif platform == "darwin":
+    from Quartz import CGWindowListCopyWindowInfo, kCGNullWindowID, kCGWindowListOptionAll
 
 
 class WindowCapture:
@@ -10,16 +15,19 @@ class WindowCapture:
         self.wait_time = 1 / capture_rate
 
     def screenshot(self):
-        hwnd = win32gui.FindWindow(None, self.window_name)
-        if not hwnd:
-            raise Exception('Window not found: ' + self.window_name)
+        if platform == "win32":
+            hwnd = win32gui.FindWindow(None, self.window_name)
+            if not hwnd:
+                raise Exception('Window not found: ' + self.window_name)
 
-        left, top, right, bot = win32gui.GetClientRect(hwnd)
-        x, y = win32gui.ClientToScreen(hwnd, (left, top))
+            left, top, right, bot = win32gui.GetClientRect(hwnd)
+            x, y = win32gui.ClientToScreen(hwnd, (left, top))
 
-        return cv2.cvtColor(
-            np.asarray(
-                pyautogui.screenshot(
-                    region=(x, y,
-                            *win32gui.ClientToScreen(hwnd, (right - x, bot - y))))),
-            cv2.COLOR_RGB2BGR)
+            return cv2.cvtColor(
+                np.asarray(
+                    pyautogui.screenshot(
+                        region=(x, y,
+                                *win32gui.ClientToScreen(hwnd, (right - x, bot - y))))),
+                cv2.COLOR_RGB2BGR)
+        elif platform == "darwin":
+            return cv2.cvtColor(np.asarray(pyautogui.screenshot()), cv2.COLOR_RGB2BGR)
