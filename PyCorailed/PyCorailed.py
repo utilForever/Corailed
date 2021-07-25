abafc37ce0e61b5458e77a870c62073c3341b662
Chris Ohk <utilforever@gmail.com>
2021-07-26 01:57:39 +0900

    docs: Add comment to improve readability

--- PyCorailed/PyCorailed.py
+++ PyCorailed/PyCorailed.py
@@ -10,6 +10,7 @@ ESC_KEY = 27
 FRAME_RATE = 10
 SLEEP_TIME = 1 / FRAME_RATE
 
+# Initialize the window
 capture = window.WindowCapture("Unrailed!", FRAME_RATE)
 
 # Create map
@@ -17,28 +18,38 @@ game_map = map.Map(20, 36, 22, 16, 10)
 game_map.init_matrix()
 
 while True:
+    # Draw matrix and image of game map
     game_map.draw_matrix()
     im = game_map.draw_image()
+
+    # Show game map
     cv2.imshow("Unrailed! Game Map", im)
 
+    # Initialize matrix
     game_map.init_matrix()
 
     start = time.time()
 
+    # Take screenshot from window and cut it
     frame = capture.screenshot()
     im2 = functions.cut(frame)
 
+    # Recognize objects on the image
     functions.test(im2, game_map)
 
+    # Draw contours of objects and grid
     functions.draw(im2)
     functions.grid(im2) 
 
+    # Show game clone
     cv2.imshow("Unrailed! Clone", im2)
 
+    # Wait for ESC key
     delta = time.time() - start
     if delta < SLEEP_TIME:
         time.sleep(SLEEP_TIME - delta)
 
+    # Process key events
     key = cv2.waitKey(1) & 0xFF
     if key == ESC_KEY:
         break
